76450e4984da3f056d3f2c3e8b3059e3cb0dd5b4
David <david.alekna@msknote.com>
2018-11-16 17:51:09 +0000

    theme object in place

--- src/helpers/theme.js
+++ src/helpers/theme.js
@@ -1,1 +1,17 @@
-export default {};
+export default {
+  one: {
+    primary: Object.assign('#333', {
+      200: '',
+      400: '',
+    }), // 5-10 of lighter and darker shades
+    neutral: Object.assign('#CCC', {
+      200: '',
+    }), // 8-10 shades
+    accents: {
+      feature: {}, // 5-10
+      success: {}, // 5-10
+      danger: {}, // 5-10
+      warning: {}, // 5-10
+    },
+  },
+};
